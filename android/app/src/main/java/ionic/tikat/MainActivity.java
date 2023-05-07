3df2f5360f14892e3d2f2cdc1ad342f28ddebeb7
ugurcansoruc <ugurcan.soruc@gmail.com>
2023-05-07 16:12:32 +0300

    TikAt App projesinin initlenmiştir.

---
+++ android/app/src/main/java/ionic/tikat/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.tikat;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.tikat',
+  appName: 'tikat',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
